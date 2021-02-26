637058b2c26ad277c53b31fbf2ce52859fc95993
Wirawat Jaiarree <katak6565@gmail.com>
2021-02-27 00:17:56 +0700

    [ADD] responsive About Page

--- component/skills.js
+++ component/skills.js
@@ -6,9 +6,10 @@ const Skills = ({ id }) => {
       icons: [
         { title: "HTML", icon: "html5" },
         { title: "CSS", icon: "css3-alt" },
-        { title: "Java Script", icon: "js-square" },
+        { title: "JS", icon: "js-square" },
         { title: "Vue.js", icon: "vuejs" },
         { title: "React", icon: "react" },
+        { title: "Next.js", icon: "/images/nextjs-logo.png" },
       ],
     },
   ];
@@ -26,11 +27,10 @@ const Skills = ({ id }) => {
   const design = [
     {
       icons: [
-        { title: "Photoshop", icon: "fas fa-database" },
-        { title: "Illustrator", icon: "fab fa-java" },
-        { title: "Figma", icon: "fab fa-node-js" },
-        { title: "Git & Github", icon: "fab fa-github-square" },
-        { title: "Docker", icon: "fab fa-docker" },
+        { title: "Photoshop", icon: "/images/ps.png" },
+        { title: "Illustrator", icon: "/images/Ai.png" },
+        { title: "Premire Pro", icon: "/images/Pr.png" },
+        { title: "Figma", icon: "/images/figma.png" },
       ],
     },
   ];
@@ -41,18 +41,15 @@ const Skills = ({ id }) => {
           <div>
             {skills.map((i, index) => (
               <div key={index}>
-                <div className="row" style={{ marginLeft: "20px" }}>
+                <div className="row">
                   {i.icons.map((s, i) => (
                     <div>
-                      <div
-                        className="column"
-                        key={i}
-                        style={{ margin: "20px" }}
-                      >
-                        <i
-                          className={`fab fa-${s.icon}`}
-                          style={{ color: "#d18585", fontSize: "30px" }}
-                        ></i>
+                      <div className="column" key={i}>
+                        {s.icon.substring(0, 1) == "/" ? (
+                          <img src={`${s.icon}`} className="icon"></img>
+                        ) : (
+                          <i className={`fab fa-${s.icon}`}></i>
+                        )}
 
                         <p className="font">{s.title}</p>
                       </div>
@@ -64,20 +61,48 @@ const Skills = ({ id }) => {
           </div>
         </div>
         <style jsx>{`
+          i {
+            color: #d18585;
+            font-size: 35px;
+          }
+          .icon {
+            width: 60px;
+          }
           .row {
             display: flex;
             justify-content: center;
             align-items: center;
           }
           .column {
+            margin: 20px;
             display: flex;
             justify-content: center;
             align-items: center;
             flex-direction: column;
           }
           .font {
-            font-size: 15px;
+            font-size: 13px;
             font-weight: 500;
+          }
+          @media only screen and (max-width: 768px) {
+            i {
+              color: #d18585;
+              font-size: 25px;
+            }
+            .font {
+              font-size: 10px;
+              font-weight: 500;
+            }
+            .icon {
+              width: 28px;
+            }
+            .column {
+              margin: 10px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              flex-direction: column;
+            }
           }
         `}</style>
       </Fragment>
@@ -90,20 +115,87 @@ const Skills = ({ id }) => {
           <div>
             {dnt.map((i, index) => (
               <div key={index}>
-                <div className="row" style={{ marginLeft: "20px" }}>
+                <div className="row">
+                  {i.icons.map((s, i) => (
+                    <div>
+                      <div className="column" key={i}>
+                        <i className={`${s.icon}`}></i>
+
+                        <p className="font">{s.title}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <style jsx>{`
+          i {
+            color: #d18585;
+            font-size: 30px;
+          }
+          .icon {
+            width: 50px;
+          }
+          .row {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          .column {
+            margin: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+          }
+          .font {
+            font-size: 13px;
+            font-weight: 500;
+          }
+          @media only screen and (max-width: 768px) {
+            i {
+              color: #d18585;
+              font-size: 25px;
+            }
+            .font {
+              font-size: 10px;
+              font-weight: 500;
+            }
+            .icon {
+              width: 28px;
+            }
+            .column {
+              margin: 10px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              flex-direction: column;
+            }
+          }
+        `}</style>
+      </Fragment>
+    );
+  }
+  if (id == "design") {
+    return (
+      <Fragment>
+        <div className="row">
+          <div>
+            {design.map((i, index) => (
+              <div key={index}>
+                <div className="row">
                   {i.icons.map((s, i) => (
                     <div>
                       <div
                         className="column"
                         key={i}
-                        style={{ margin: "20px" }}
+                        style={{ margin: "15px" }}
                       >
-                        <i
-                          className={`${s.icon}`}
-                          style={{ color: "#d18585", fontSize: "30px" }}
-                        ></i>
+                        <img src={`${s.icon}` } className="icon" ></img>
 
-                        <p className="font" style={{ fontSize: "13px" }}>
+                        <p className="font">
                           {s.title}
                         </p>
                       </div>
@@ -115,28 +207,54 @@ const Skills = ({ id }) => {
           </div>
         </div>
         <style jsx>{`
+          i {
+            color: #d18585;
+            font-size: 30px;
+          }
+          .icon {
+            width: 40px;
+          }
           .row {
             display: flex;
             justify-content: center;
             align-items: center;
           }
           .column {
+            margin: 20px;
             display: flex;
             justify-content: center;
             align-items: center;
             flex-direction: column;
           }
           .font {
-            font-size: 15px;
+            font-size: 13px;
             font-weight: 500;
+          }
+          @media only screen and (max-width: 768px) {
+            i {
+              color: #d18585;
+              font-size: 25px;
+            }
+            .font {
+              font-size: 10px;
+              font-weight: 500;
+            }
+            .icon {
+              width: 28px;
+            }
+            .column {
+              margin: 10px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              flex-direction: column;
+            }
           }
         `}</style>
       </Fragment>
     );
   }
 
-  return <Fragment>
-    test
-  </Fragment>;
+  return <Fragment>test</Fragment>;
 };
 export default Skills;
--- pages/about.js
+++ pages/about.js
@@ -3,11 +3,7 @@ import Layout from "../component/Layout";
 import Skills from "../component/skills";
 import Footer from "../component/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faSchool,
-  faCode,
-  faHospitalSymbol,
-} from "@fortawesome/free-solid-svg-icons";
+import { faSchool } from "@fortawesome/free-solid-svg-icons";
 const About = () => {
   return (
     <Fragment>
@@ -15,19 +11,11 @@ const About = () => {
         <Layout page="about">
           <div className="motion">
             <div className="container">
-              <img
-                src="/images/profile.png"
-                width="400px"
-                height="auto"
-                className="profilePic"
-                alt="profile picture"
-              ></img>
+              <img src="/images/profile.png" className="profilePic"></img>
               <div className="column-con">
                 <div>
                   <div>
-                    <h1 className="text" style={{ margin: "0px" }}>
-                      Hello,{" "}
-                    </h1>
+                    <h1 className="text">Hello</h1>
                     <div className="row-con">
                       <h1
                         className="text"
@@ -49,13 +37,7 @@ const About = () => {
                     </div>
                   </div>
                 </div>
-                <div
-                  style={{
-                    backgroundColor: "#D18585",
-                    width: "460px",
-                    height: "3px",
-                  }}
-                >
+                <div className="line">
                   <div>
                     <p style={{ color: "#7B7B7B" }}>
                       - I’ m sophomore student in Computer Science at King
@@ -73,7 +55,7 @@ const About = () => {
             </div>
           </div>
           <div>
-            <div className="container">
+            <div className="container1">
               <div className="info">
                 <div className="container-card">
                   <div className="row">
@@ -94,7 +76,8 @@ const About = () => {
 
                   <div className="card" style={{ marginTop: "2rem" }}>
                     <p>King Mongkut's University of Technology Thonburi</p>
-                    <p>2019 - Present</p>
+
+                    <p>(2019 - Present)</p>
                     <p>I'm currently studying bachelor's second year of</p>
                     <p>Computer Science at School of Information Technology</p>
                     <p>Current GPAX : 3.65</p>
@@ -115,77 +98,55 @@ const About = () => {
                       }}
                     ></i>
                   </div>
-                  <div
-                    className="card"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "flex-start",
-                      height: "130px",
-                      padding: "0px",
-                    }}
-                  >
-                    <div
-                      className="row"
-                      style={{ paddingLeft: "20px", height: "30px" }}
-                    >
-                      <h3 style={{ color: "#D18585", marginTop: "10px" }}>
+                  <div className="card1">
+                    <div className="row" style={{ paddingLeft: "20px" }}>
+                      <h3 style={{ color: "#D18585", margin: "0px" }}>
                         Frontend
                       </h3>
-                      <h3 style={{ marginLeft: "5px", marginTop: "10px" }}>
+                      <h3
+                        style={{
+                          marginLeft: "5px",
+                          margin: "0px  0px 0px 5px",
+                        }}
+                      >
                         Development
                       </h3>
                     </div>
                     <Skills id={"font"}></Skills>
-                    <br />
                   </div>
                   <br />
-                  <div
-                    className="card"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "flex-start",
-                      height: "130px",
-                      padding: "0px",
-                    }}
-                  >
-                    <div
-                      className="row"
-                      style={{ paddingLeft: "20px", height: "30px" }}
-                    >
-                      <h3 style={{ color: "#D18585", marginTop: "10px" }}>
-                        Backend and Tools
+                  <div className="card1">
+                    <div className="row" style={{ paddingLeft: "20px" }}>
+                      <h3 style={{ color: "#D18585", margin: "0px" }}>
+                        DB, Backend and Tools
                       </h3>
-                      <h3 style={{ marginLeft: "5px", marginTop: "10px" }}>
+                      <h3
+                        style={{
+                          marginLeft: "5px",
+                          margin: "0px  0px 0px 5px",
+                        }}
+                      >
                         Development
                       </h3>
                     </div>
                     <Skills id={"back"} />
                   </div>
                   <br />
-                  <div
-                    className="card"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "flex-start",
-                      height: "130px",
-                      padding: "0px",
-                    }}
-                  >
-                    <div
-                      className="row"
-                      style={{ paddingLeft: "20px", height: "30px" }}
-                    >
-                      <h3 style={{ color: "#D18585", marginTop: "10px" }}>
-                       Design
+                  <div className="card1">
+                    <div className="row" style={{ paddingLeft: "20px" }}>
+                      <h3 style={{ color: "#D18585", margin: "0px" }}>
+                        Graphic
                       </h3>
-                      <h3 style={{ marginLeft: "5px", marginTop: "10px" }}>
-                        Development
+                      <h3
+                        style={{
+                          marginLeft: "5px",
+                          margin: "0px  0px 0px 5px",
+                        }}
+                      >
+                        Skills
                       </h3>
                     </div>
-                    <Skills id={"back"} />
+                    <Skills id={"design"} />
                   </div>
                 </div>
               </div>
@@ -195,6 +156,15 @@ const About = () => {
         </Layout>
       </div>
       <style jsx>{`
+        .line {
+          background-color: #d18585;
+          width: 460px;
+          height: 3px;
+        }
+        .profilePic {
+          width: 400px;
+        }
+
         .row {
           display: flex;
           flex-direction: row;
@@ -205,7 +175,6 @@ const About = () => {
         }
 
         .card {
-          height: 100%;
           display: flex;
           justify-content: center;
           background: white;
@@ -216,6 +185,19 @@ const About = () => {
           border: none;
           box-shadow: 3px 3px 5px 3px #ccc;
           padding: 0 10px 5px 30px;
+          flex-direction: column;
+        }
+        .card1 {
+          height: 140px;
+          display: flex;
+
+          background: white;
+          transition: 0.3s;
+          width: 500px;
+          padding: 10px;
+          border-radius: 2vh;
+          border: none;
+          box-shadow: 3px 3px 5px 3px #ccc;
           flex-direction: column;
         }
         .motion {
@@ -239,6 +221,13 @@ const About = () => {
           flex-direction: column;
         }
         .container {
+          min-height: 50vh;
+          display: flex;
+          justify-content: center;
+          align-items: flex-start;
+          flex-direction: row;
+        }
+        .container1 {
           min-height: 50vh;
           display: flex;
           justify-content: center;
@@ -269,6 +258,7 @@ const About = () => {
         }
         .text {
           font-size: 3rem;
+          margin: 0px;
         }
 
         @keyframes example {
@@ -286,6 +276,94 @@ const About = () => {
           }
           100% {
             opacity: 100%;
+          }
+        }
+        @media only screen and (max-width: 768px) {
+          .info {
+            display: flex;
+            flex-direction: column;
+          }
+          .profilePic {
+            width: 300px;
+          }
+          .text {
+            text-align: center;
+            font-size: 2.3rem;
+          }
+          .container {
+            height: 90vh;
+            width: 100vw;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            flex-direction: column;
+          }
+          .container1 {
+            height: 135vh;
+            width: 100vw;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            flex-direction: column;
+          }
+          .column-con {
+            margin-top: 60px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+          }
+          .line {
+            background-color: #d18585;
+            width: 350px;
+            height: 3px;
+          }
+
+          .container-card {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            margin: 0;
+          }
+          .card {
+            display: flex;
+            padding: 5px;
+            justify-content: center;
+            background: white;
+            transition: 0.3s;
+            width: 350px;
+            border-radius: 2vh;
+            border: none;
+            box-shadow: 3px 3px 5px 3px #ccc;
+
+            flex-direction: column;
+          }
+          p {
+            padding: 0px;
+            margin: 5px;
+            text-align: center;
+          }
+          .card1 {
+            height: 110px;
+            width: 350px;
+            display: flex;
+            justify-content: flex-start;
+            background: white;
+            transition: 0.3s;
+
+            padding: 50px;
+            border-radius: 2vh;
+            border: none;
+            box-shadow: 3px 3px 5px 3px #ccc;
+            padding: 0 10px 5px 30px;
+            flex-direction: column;
+            padding: 0px;
+          }
+          .row {
+            display: flex;
+            margin-top: 10px;
+            flex-direction: row;
           }
         }
       `}</style>
