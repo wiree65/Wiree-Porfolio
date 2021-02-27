c216bfbe3dc06a78c3e5395c48df085bace4ba4a
Wirawat Jaiarree <katak6565@gmail.com>
2021-02-27 15:12:27 +0700

    [ADD] act,contact,exp

---
+++ pages/experiences.js
@@ -0,0 +1,28 @@
+import { Fragment, useState, useEffect } from "react";
+
+import Layout from "../component/Layout";
+const General = (props) => {
+
+  return (
+    <Fragment>
+        <Layout page="experiences">
+      <div className="container">
+       เดี๋ยวมาทำหลังมิดเทอมค้าบ
+       
+      </div>
+      </Layout>
+      <style jsx>{`
+        .container {
+            height:90vh;
+            width:100vw;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+        }
+      `}</style>
+    </Fragment>
+  );
+};
+
+export default General;
