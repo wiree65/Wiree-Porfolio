a467de11d4e6fe39bcf827d79a3a97aee02cc48c
Wirawat Jaiarree <katak6565@gmail.com>
2021-03-03 23:20:00 +0700

    [UPDATE] content [ADD] picture burger

--- component/graphic.js
+++ component/graphic.js
@@ -1,19 +1,155 @@
 import { Fragment, useState, useEffect } from "react";
+import React from "react";
+
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@material-ui/core";
 const Web = (props) => {
+  const [popup, setPopup] = React.useState(0);
+
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const setRenderPopup = (number) => {
+    setPopup(number);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  useEffect(() => {}, []);
+  function Example(props) {
+    var item;
+    if (popup == 0) {
+      item = [
+        {
+          image: "./images/Graphic/burger/1.png",
+        },
+        {
+          image: "./images/Graphic/burger/2.png",
+        },
+        {
+          image: "./images/Graphic/burger/3.png",
+        },
+        {
+          image: "./images/Graphic/burger/5.png",
+        },
+        {
+          image: "./images/Graphic/burger/6.png",
+        },
+        {
+          image: "./images/Graphic/burger/7.jpg",
+        },
+        
+      ];
+    } else {
+      item = [
+        {
+          image: "./images/project/eduroom/projectCSB/1.png",
+        },
+        {
+          image: "./images/project/eduroom/projectCSB/2.png",
+        },
+        {
+          image: "./images/project/eduroom/projectCSB/3.png",
+        },
+        {
+          image: "./images/project/eduroom/projectCSB/4.png",
+        },
+        {
+          image: "./images/project/eduroom/projectCSB/5.png",
+        },
+        {
+          image: "./images/project/eduroom/projectCSB/6.png",
+        },
+        {
+          image: "./images/project/eduroom/projectCSB/7.png",
+        },
+
+        {
+          image: "./images/project/eduroom/projectCSB/8.png",
+        },
+        {
+          image: "./images/project/eduroom/projectCSB/9.png",
+        },
+        {
+          image: "./images/project/eduroom/projectCSB/10.png",
+        },
+        {
+          image: "./images/project/eduroom/projectCSB/11.png",
+        },
+      ];
+    }
+
+    return (
+      <Carousel>
+        {item.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Carousel>
+    );
+  }
+  function Item(props) {
+    return (
+      <Paper>
+        <img src={`${props.item.image}`} style={{ width: "100%" }}>
+          {}
+        </img>
+      </Paper>
+    );
+  }
   const skills = [
     {
       icons: [
-        { title: "Burger Joker" },
-        { title: "Yum Yod Chai" },
-        { title: "CS@SIT Open House" },
-        { title: "GEN351 Poster" },
-        { title: "Animation 2D" },
+        {
+          intro: "Burger Joker",
+          content:
+            "It's restaurant that I design a logo, menu, promotions",
+          year: "2020",
+          img: "./images/Graphic/burger/2.png",
+          picinhover: "./images/Graphic/burger/7.jpg",
+
+        },
+        
       ],
     },
   ];
   return (
     <Fragment>
       <div className="container1">
+        <div>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle
+              style={{
+                display: "flex",
+                alignContent: "center",
+                justifyContent: "center",
+              }}
+            >
+              <h3> {skills[0].icons[popup].title}</h3>
+            </DialogTitle>
+            <DialogContent>
+           
+              {Example()}
+           
+              <br />
+              <DialogContentText id="alert-dialog-description">
+                {skills[0].icons[popup].content == "" ? (
+                  <div>{}</div>
+                ) : (
+                  <p><storng style={{fontSize:'20px'}}>Detail: </storng>{skills[0].icons[popup].content}</p>
+                )}
+               
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions></DialogActions>
+          </Dialog>
+        </div>
         <div>
           {skills.map((i, index) => (
             <div key={index}>
@@ -23,21 +159,42 @@ const Web = (props) => {
                     <div className="row">
                       <div className="container">
                         <img
-                          src="./images/project/eduroom/1.png"
-                          style={{ width: "100%", borderRadius: "32px" }}
+                          src={`${s.img}`}
+                          style={{ width: "190px", borderRadius: "32px" }}
                         ></img>
 
-                        <h3 className="font">{s.title}</h3>
-                        <p>2020</p>
-<br/>
+                        <h3 className="font">{s.intro}</h3>
+                        <p>{s.year}</p>
+
+                        <br />
                         <div className="hoverdiv">
-                          <h1 className="font">{s.title}</h1>
-                          <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                          </p>
+                          <div className="content">
+                          <h5> {s.intro}</h5>
+                    
+                          <img
+                          src={`${s.picinhover}`}
+                          style={{ width: "150px",borderRadius:'10px' }}
+                        ></img>
+                            <p>{s.content}</p>
+                          </div>
+
+                          <button
+                            className="button"
+                            onClick={() => {
+                              handleClickOpen();
+                              setRenderPopup(i);
+                            }}
+                          >
+                            
+                            <span
+                              style={{
+                                fontSize: "14px",
+                                backgroundColor: "transparent",
+                              }}
+                            >
+                              see more information
+                            </span>
+                          </button>
                         </div>
                       </div>
                     </div>
@@ -50,27 +207,62 @@ const Web = (props) => {
       </div>
 
       <style jsx>{`
+        h1 {
+          color: #333333;
+        }
+        h5 {
+          color: #6d6d6d;
+        }
+        .p {
+        }
+        .button {
+          postion: absolute;
+          background-color: #d18585;
+          width: 200px;
+          height: 40px;
+          border-radius: 30px;
+          color: white;
+          // margin: 10px;
+          border: none;
+          outline: none;
+        }
+        .button:hover {
+          transition: 0.5s;
+          color: #d18585;
+          border: solid;
+          background-color: white;
+          opacity: 1;
+          right: 1;
+        }
+        p {
+          color: #6d6d6d;
+
+          padding: 0;
+          margin: 10px;
+        }
+        .content {
+          margin: 0;
+          padding: 5px;
+        }
+        .link {
+          color: #d18585;
+        }
         .container {
           width: 320px;
+          height:300px;
           box-shadow: 3px 3px 5px 3px #ccc;
           margin: 20px;
           border-radius: 32px;
           transform: scale(1);
-
           transition: all 0.3s ease-in-out;
         }
 
         .hoverdiv {
-          display:flex;
-          justify-content:center;
-          align-items:center;
-          // flex-direction:column
           position: absolute;
           top: 0;
           left: 0;
           width: 100%;
           height: 100%;
-          padding: 24px;
           background: white;
           color: black;
           opacity: 0;
@@ -96,7 +288,7 @@ const Web = (props) => {
         }
 
         .container1 {
-          width: 100vw;
+          // width: 100vw;
           display: flex;
           justify-content: flex-start;
           align-items: center;
--- pages/contact.js
+++ pages/contact.js
@@ -12,30 +12,41 @@ const General = (props) => {
             <p className="font">
               I’m looking forward to start a project with you!
             </p>
-            <p style={{color:"#7b7b7b"}}>
-             Please feel free to contact me
-            </p>
+            <p style={{ color: "#7b7b7b" }}>Please feel free to contact me</p>
           </div>
           <div className="row">
-            <div className="card">
-              <div style={{ display: "flex", flexDirection: "row",alignItems:'center', }}>
-              <i
-                  className="fab fa-facebook-square"
+            <a href="https://fb.me/katak56">
+              <div className="card">
+                <div
                   style={{
                     display: "flex",
-                    color: "#D18585",
-                    fontSize: "2rem",
-                    marginRight: "1rem",
+                    flexDirection: "row",
+                    alignItems: "center",
                   }}
-                ></i>
-                <p style={{fontWeight:700}}> Facebook:</p>
-               
+                >
+                  <i
+                    className="fab fa-facebook-square"
+                    style={{
+                      display: "flex",
+                      color: "#D18585",
+                      fontSize: "2rem",
+                      marginRight: "1rem",
+                    }}
+                  ></i>
+                  <p style={{ fontWeight: 700 }}> Facebook:</p>
+                </div>
+                <p style={{ color: "#333333" }}>fb.me/katak56</p>
               </div>
-              <p style={{color:'#333333'}}>กาต๊าก กะตัก</p>
-            </div>
+            </a>
             <div className="card">
-              <div style={{ display: "flex", flexDirection: "row",alignItems:'center', }}>
-              <i
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <i
                   className="fas fa-phone-square-alt"
                   style={{
                     display: "flex",
@@ -44,14 +55,19 @@ const General = (props) => {
                     marginRight: "1rem",
                   }}
                 ></i>
-                <p style={{fontWeight:700}}> Tel:</p>
-               
+                <p style={{ fontWeight: 700 }}> Tel:</p>
               </div>
-              <p style={{color:'#333333'}}>091-495-1456</p>
+              <p style={{ color: "#333333" }}>091-495-1456</p>
             </div>
             <div className="card">
-              <div style={{ display: "flex", flexDirection: "row",alignItems:'center', }}>
-              <i
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <i
                   className="fas fa-envelope"
                   style={{
                     display: "flex",
@@ -60,14 +76,21 @@ const General = (props) => {
                     marginRight: "1rem",
                   }}
                 ></i>
-                <p style={{fontWeight:700}}> Email Adress:</p>
-               
+                <p style={{ fontWeight: 700 }}> Email Adress:</p>
               </div>
-              <p style={{color:'#333333'}}>Katak6565@gmail.com</p>
+              <p style={{ color: "#333333" }}>Katak6565@gmail.com</p>
             </div>
+            <a href="https://github.com/wiree65">
+
             <div className="card">
-              <div style={{ display: "flex", flexDirection: "row",alignItems:'center', }}>
-              <i
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <i
                   className="fab fa-github-square"
                   style={{
                     display: "flex",
@@ -76,16 +99,13 @@ const General = (props) => {
                     marginRight: "1rem",
                   }}
                 ></i>
-                <p style={{fontWeight:700}}> Github:</p>
-               
+                <p style={{ fontWeight: 700 }}> Github:</p>
               </div>
-              <p style={{color:'#333333'}}>github.com/wiree65</p>
+              <p style={{ color: "#333333" }}>github.com/wiree65</p>
             </div>
-       
+            </a>
           </div>
         </div>
-     
-      
       </Layout>
       <Footer></Footer>
       <style jsx>{`
@@ -100,6 +120,19 @@ const General = (props) => {
           border-radius: 2vh;
           border: none;
           box-shadow: 3px 3px 5px 3px #ccc;
+          padding: 0 10px 5px 30px;
+          flex-direction: column;
+        }
+        .card:hover {
+          display: flex;
+          justify-content: center;
+          background: white;
+          transition: 0.3s;
+          width: 300px;
+          padding: 50px;
+          border-radius: 2vh;
+          border: none;
+          box-shadow: 3px 5px 15px 3px #ccc;
           padding: 0 10px 5px 30px;
           flex-direction: column;
         }
@@ -148,7 +181,6 @@ const General = (props) => {
             color: #7b7b7b;
           }
           h1 {
-            
             color: #d18585;
             font-size: 40px;
             margin-top: 50px;
